b81a0eaa3114d6cfaa802b0eb4fec34f412af60b
trishncl <sanosatrishanicole@gmail.com>
2024-08-21 11:34:01 +0800

    Initialized Repository

---
+++ Exercise Set 01/escseq.py
@@ -0,0 +1,8 @@
+multiple = [7, 14, 21, 28, 35]
+for x in multiple:
+    if x % 2 != 0:
+     print(x)
+    else:
+     print('\t', x)
+
+    
---
+++ Exercise Set 01/tempconvert.py
@@ -0,0 +1,4 @@
+
+cel = input("Enter Celsius value: ")
+far = float(cel) * 9/5 + 32
+print(format(float(cel), '.2f'), "Celsius is", format(far, '.2f'), "Farenheit")
