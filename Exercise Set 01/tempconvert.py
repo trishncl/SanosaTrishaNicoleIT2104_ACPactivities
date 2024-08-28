1832ef6e381e20f0350f78cb714ecc812bdcea67
trishncl <sanosatrishanicole@gmail.com>
2024-08-28 11:06:55 +0800

    001 | Python Set 1

---
+++ Exercise Set 01/tempconvert.py
@@ -0,0 +1,3 @@
+cel = float(input("Enter Celsius value: "))
+far = cel * 9/5 + 32
+print(format(cel, '.2f'), "Celsius is", format(far, '.2f'), "Farenheit")
--- SañosaTrishaNicoleIT2104_ACPactivities/Exercise Set 01/Exercise Set 01/tempconvert.py
+++
@@ -1,3 +0,0 @@
-cel = input("Enter Celsius value: ")
-far = float(cel) * 9/5 + 32
-print(format(float(cel), '.2f'), "Celsius is", format(far, '.2f'), "Farenheit")
