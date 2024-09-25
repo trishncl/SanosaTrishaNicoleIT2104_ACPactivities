8206a02912e80ef703b2ac8a171eec043be70d8b
trishncl <sanosatrishanicole@gmail.com>
2024-09-25 11:19:09 +0800

    002 | 25-09-24

--- Laboratory Activities/Laboratory Activity 2 - 25-September-2024/lab2_item2.py
+++ Laboratory Activities/Laboratory Activity 2 - 25-September-2024/lab2_item2.py
@@ -1,15 +1,16 @@
 while True:
-    amount = float(input("Enter the total purchase amount: "))
-    print(f"Initial Purchase Amount: {amount:.2f}")
+    amount = int(input("Enter the total purchase amount: "))
+    amount_f = float(amount)
+    print(f"Initial Purchase Amount: {amount_f:.2f}")
 
     if amount > 5000:
-        discount = amount * 0.10
+        discount = amount_f * 0.10
     else:
-        discount = amount * 0.05
+        discount = amount_f * 0.05
 
     print(f"Discount: {discount:.2f}")
 
-    final_price = amount - discount
+    final_price = amount_f - discount
     print(f"Final Price: {final_price:.2f}")
 
     choice = input("Do you want to try again? (yes/no): ")
